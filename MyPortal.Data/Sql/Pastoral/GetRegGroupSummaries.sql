ec25b239d6c1b76253bd5c7bf07ad7b6349fc822
Rowan Richards <rwnrchrds@gmail.com>
2026-05-11 13:11:29 +0100

    PR comments

--- MyPortal.Data/Sql/Pastoral/GetRegGroupSummaries.sql
+++ MyPortal.Data/Sql/Pastoral/GetRegGroupSummaries.sql
@@ -3,11 +3,12 @@
     SG.Code,
     SG.[Description]    AS [Name],
     SG.Active,
-    YG.[Name]           AS YearGroupName,
+    YSG.[Description]   AS YearGroupName,
     MainName.[Name]     AS MainSupervisorName
     FROM dbo.RegGroups                    AS RG
     JOIN dbo.StudentGroups                AS SG  ON SG.Id = RG.StudentGroupId
     JOIN dbo.YearGroups                   AS YG  ON YG.Id = RG.YearGroupId
+    JOIN dbo.StudentGroups                AS YSG ON YSG.Id = YG.StudentGroupId
     LEFT JOIN dbo.StudentGroupSupervisors AS SGS ON SGS.Id = SG.MainSupervisorId
     LEFT JOIN dbo.StaffMembers            AS SM  ON SM.Id = SGS.SupervisorId
     OUTER APPLY dbo.fn_person_get_name(SM.PersonId, 2, 0, 1) AS MainName
